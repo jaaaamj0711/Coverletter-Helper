cda84798b0bc198d7e9af9511773c71f79153f94
pmsk98 <pmsk98@naver.com>
2020-10-14 15:49:33 +0900

    assert plus

--- job_answer_preprocess.py
+++ job_answer_preprocess.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 # str.replace 함수 정의 (특정 패턴 공백으로 처리)
 def str_replace(text, patterns: list, replace=" "):
+        
+        assert isinstance(text,pd.core.series.Series)
+        assert isinstance(patterns,list)
 
         for pattern in patterns:
                 text = text.str.replace(pattern, replace)
@@ -12,6 +15,9 @@ def str_replace(text, patterns: list, replace=" "):
 
 # re.sub 함수 정의 (특정 정규식 패턴 공백으로 처리)
 def re_sub(text, patterns: dict):
+        
+        assert isinstance(text,pd.core.series.Series)
+        assert isinstance(patterns,dict)
 
         for pattern in patterns:
                 text[text.notna()] = text[text.notna()].apply(lambda x: re.sub(pattern, patterns[pattern], x))
