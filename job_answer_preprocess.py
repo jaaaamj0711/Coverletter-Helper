9874f3eb852e975995ac920de791b346d95e756f
KWON DO YUN <0727ehdbs@gmail.com>
2020-10-14 15:28:47 +0900

    Job Answer Preprocess

---
+++ job_answer_preprocess.py
@@ -0,0 +1,20 @@
+import re
+import numpy as np
+import pandas as pd
+
+# str.replace 함수 정의 (특정 패턴 공백으로 처리)
+def str_replace(text, patterns: list, replace=" "):
+
+        for pattern in patterns:
+                text = text.str.replace(pattern, replace)
+
+        return text
+
+# re.sub 함수 정의 (특정 정규식 패턴 공백으로 처리)
+def re_sub(text, patterns: dict):
+
+        for pattern in patterns:
+                text[text.notna()] = text[text.notna()].apply(lambda x: re.sub(pattern, patterns[pattern], x))
+
+        return text
+~   
