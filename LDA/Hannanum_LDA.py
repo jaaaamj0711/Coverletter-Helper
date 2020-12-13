543aa0e945faf88729037c080686fcefed96b534
jaaaamj0711 <jaaaamj0711@gmail.com>
2020-12-13 08:43:23 +0000

    add nouns

--- LDA/Hannanum_LDA.py
+++ LDA/Hannanum_LDA.py
@@ -11,3 +11,12 @@ hannanum=Hannanum()
 
 x_list = data['답변']
 x_list
+
+data_word=[]
+for i in range(len(x_list)):
+    try:
+        data_word.append(hannanum.nouns(x_list[i]))
+    except Exception as e:
+        continue
+
+Data_list=x_list.values.tolist()
