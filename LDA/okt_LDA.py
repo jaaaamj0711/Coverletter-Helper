e0aa53fcea6cf86ca66d873d26a7c75d02143dc4
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-13 08:40:30 +0000

    new data_word

--- LDA/okt_LDA.py
+++ LDA/okt_LDA.py
@@ -11,3 +11,10 @@ okt=Okt()
 
 x_list = data['답변']
 x_list
+
+data_word=[]
+for i in range(len(x_list)):
+    try:
+        data_word.append(okt.nouns(x_list[i]))
+    except Exception as e:
+        continue
