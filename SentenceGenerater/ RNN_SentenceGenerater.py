6a72cb6353235f34834369aa0080aae22f20cfcb
jaaaamj0711 <jaaaamj0711@gmail.com>
2020-12-13 09:03:21 +0000

    sequences result

--- SentenceGenerater/ RNN_SentenceGenerater.py
+++ SentenceGenerater/ RNN_SentenceGenerater.py
@@ -17,3 +17,16 @@ print('총 샘플의 개수 : {}'.format(len(headline))) # 현재 샘플의 개�
 
 headline = [n for n in headline if n != "Unknown"] # Unknown 값을 가진 샘플 제거
 print('노이즈값 제거 후 샘플의 개수 : {}'.format(len(headline))) # 제거 후 샘플의 개수
+
+text = headline
+text[:5]
+
+t = Tokenizer()
+t.fit_on_texts(text)
+vocab_size = len(t.word_index) + 1
+print('단어 집합의 크기 : %d' % vocab_size)
+
+t.word_index
+
+sequences = list()
+
