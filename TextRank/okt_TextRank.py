19e1978dd1d95b4779f4974e83a97524df2fc113
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-13 08:17:57 +0000

    Update okt_TextRank.py

--- TextRank/okt_TextRank.py
+++ TextRank/okt_TextRank.py
@@ -11,3 +11,28 @@ import pandas as pd
 data = pd.read_csv("./jobkorea_all.csv")
 
 data = preprocess_answer(data)
+
+class SentenceTokenizer(object):
+    def __init__(self):
+        self.kkma = Kkma()
+        self.okt = Okt()
+    
+    # 텍스트를 입력으로 받아서 문장단위로 나누어 줌
+    def text_sentences(self, text):
+        sentences = self.kkma.sentences(text)
+        for idx in range(0, len(sentences)):
+            if len(sentences[idx]) <= 10:
+                sentences[idx-1] += (' ' + sentences[idx])
+                sentences[idx] = ''
+        return sentences
+
+    # 문장 단위로 입력을 받아서 명사를 출력
+    def sentences_nouns(self, sentences):
+        nouns = []
+        for sentence in sentences:
+            if sentence is not '':
+                nouns.append(' '.join([noun for noun in self.okt.nouns(str(sentence))
+                                if len(noun) > 1]))
+        return nouns 
+
+        
