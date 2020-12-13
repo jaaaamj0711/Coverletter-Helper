b80c11b52e939987bb9b4c88544128ad27e57aad
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-13 08:55:56 +0000

    packages load

---
+++ SentenceGenerater/LSTM_SentenceGenerater.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from string import punctuation
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+df = pd.read_csv('./kogpt2/dataset.txt')
+df.head()
