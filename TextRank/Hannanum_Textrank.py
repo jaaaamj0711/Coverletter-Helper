640ec9ebc64ea63fd37a02d05cf90979d6b112f9
jaaaamj0711 <jaaaamj0711@gmail.com>
2020-12-13 10:21:46 +0000

    data load

---
+++ TextRank/Hannanum_Textrank.py
@@ -0,0 +1,14 @@
+from konlpy.tag import Kkma
+from konlpy.tag import Okt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import normalize
+from preprocess_all import preprocess_answer
+import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv("/Users/doyun/2020_text_mining/jobkorea_data.csv")
+
+data = preprocess_answer(data)
+
