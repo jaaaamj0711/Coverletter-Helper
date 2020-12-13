c664def7bd23daa533ad6574078e1b6ca270a1b3
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-13 08:19:14 +0000

    Update okt_TextRank.py

--- TextRank/okt_TextRank.py
+++ TextRank/okt_TextRank.py
@@ -60,6 +60,21 @@ class GraphMatrix(object):
         self.graph_sentence = np.dot(tfidf_mat, tfidf_mat.T) # tfidf matrix 
         return self.graph_sentence # Sentence graph
 
+# TextRank 알고리즘을 구현
+class Rank(object):
+    def get_ranks(self, graph, d=0.85): 
+        A = graph
+        matrix_size = A.shape[0]
+        for id in range(matrix_size):
+            A[id, id] = 0 # diagonal 부분을 0으로
+            link_sum = np.sum(A[:,id]) # A[:, id] = A[:][id]
+            if link_sum != 0:
+                A[:, id] /= link_sum
+            A[:, id] *= -d
+            A[id, id] = 1
+        B = (1-d) * np.ones((matrix_size, 1))
+        ranks = np.linalg.solve(A, B) # 연립방정식 Ax = b
+        return {idx: r[0] for idx, r in enumerate(ranks)}
 
 
         
