061bf4c65d2dc4e2663971baf71191b49f7a466e
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-13 08:59:02 +0000

    sample count

--- SentenceGenerater/LSTM_SentenceGenerater.py
+++ SentenceGenerater/LSTM_SentenceGenerater.py
@@ -13,3 +13,8 @@ df['제목'].isnull().values.any()
 headline = [] # 리스트 선언
 headline.extend(list(df.제목.values)) # 헤드라인의 값들을 리스트로 저장
 headline[:5] # 상위 5개만 출력
+
+print('총 샘플의 개수 : {}'.format(len(headline))) # 현재 샘플의 개수
+
+headline = [n for n in headline if n != "Unknown"] # Unknown 값을 가진 샘플 제거
+print('노이즈값 제거 후 샘플의 개수 : {}'.format(len(headline))) # 제거 후 샘플의 개수
