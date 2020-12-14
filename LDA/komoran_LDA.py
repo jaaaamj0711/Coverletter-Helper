864a0a3a1063dd29803c4aeb78ff65907d7f89a7
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-14 06:33:55 +0000

    PEP8

--- LDA/komoran_LDA.py
+++ LDA/komoran_LDA.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-from konlpy.tag import Kommoran  
+from konlpy.tag import Komoran  
 from gensim import corpora, models
 from gensim.models.wrappers import LdaMallet
 from gensim.models.coherencemodel import CoherenceModel
 
-kommoran = Kommoran()
+komoran = Komoran()
 
 # 데이터 불러오기
 data = pd.read_csv("jobkorea_data.csv")
@@ -20,7 +20,7 @@ x_list
 data_word = []
 for i in range(len(x_list)):
     try:
-        data_word.append(hannanum.nouns(x_list[i]))
+        data_word.append(komoran.nouns(x_list[i]))
     except Exception as e:
         continue
 
