1192091703a00a50983e1a9de487b106fd2c186f
KWON DO YUN <0727ehdbs@gmail.com>
2020-12-14 07:34:00 +0000

    add sentence generation

--- SentenceGenerator/LSTM_SentenceGenerator.py
+++ SentenceGenerator/LSTM_SentenceGenerator.py
@@ -54,13 +54,29 @@ y = to_categorical(y, num_classes=vocab_size)
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Dense, LSTM
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, Dense, SimpleRNN
 
-modelRNN = Sequential()
-modelRNN.add(Embedding(vocab_size, 10, input_length=max_len-1)) # 레이블을 분리하였으므로 이제 X의 길이는 5
-modelRNN.add(SimpleRNN(32))
-modelRNN.add(Dense(vocab_size, activation='softmax'))
-modelRNN.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-modelRNN.fit(X, y, epochs=70, verbose=2)
+model.add(LSTM(128))
+model.add(Dense(vocab_size, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=24, verbose=2)
 
+def sentence_generation(model, t, current_word, n): # 모델, 토크나이저, 현재 단어, 반복할 횟수
+    init_word = current_word # 처음 들어온 단어도 마지막에 같이 출력하기위해 저장
+    sentence = ''
+    for _ in range(n): # n번 반복
+        encoded = t.texts_to_sequences([current_word])[0] # 현재 단어에 대한 정수 인코딩
+        encoded = pad_sequences([encoded], maxlen=627, padding='pre') # 데이터에 대한 패딩
+        result = model.predict_classes(encoded, verbose=0)
+    # 입력한 X(현재 단어)에 대해서 y를 예측하고 y(예측한 단어)를 result에 저장.
+        for word, index in t.word_index.items(): 
+            if index == result: # 만약 예측한 단어와 인덱스와 동일한 단어가 있다면
+                break # 해당 단어가 예측 단어이므로 break
+        current_word = current_word + ' '  + word # 현재 단어 + ' ' + 예측 단어를 현재 단어로 변경
+        sentence = sentence + ' ' + word # 예측 단어를 문장에 저장
+    # for문이므로 이 행동을 다시 반복
+    sentence = init_word + sentence
+    return sentence
+
+print(sentence_generation(model, t, '개발', 3))
+print(sentence_generation(model, t, '분석', 3))
+print(sentence_generation(model, t, '데이터', 3))
