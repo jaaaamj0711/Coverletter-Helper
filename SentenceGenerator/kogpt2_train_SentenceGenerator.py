2c9d83a4f1246c01e5b25bbfbf1f47f8c54dcf8c
박명석 <pmsk98@nate.com>
2020-12-14 08:05:53 +0000

    model parameter update

--- SentenceGenerator/kogpt2_train_SentenceGenerator.py
+++ SentenceGenerator/kogpt2_train_SentenceGenerator.py
@@ -62,4 +62,20 @@ from transformers import GPT2Config, GPT2LMHeadModel
 
 save_path = 'C:/Users/user/KoGPT2/'
 
+kogpt2_config = {
+      "initializer_range": 0.02,
+      "layer_norm_epsilon": 0.000001,
+      "n_ctx": 1024,
+      "n_embd": 768,
+      "n_head": 12,
+      "n_layer": 12,
+      "n_positions": 1024,
+      "vocab_size": 50000,
+      "activation_function": "gelu"
+}
+
+
+checkpoint = torch.load(save_path+'KoGPT2_checkpoint.tar', map_location=PU)
+
+kogpt2model = GPT2LMHeadModel(config=GPT2Config.from_dict(kogpt2_config))
 
